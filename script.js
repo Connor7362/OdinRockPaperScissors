7d10e843ad9f6d15231005610a7ed32f8cb31558
Connor7362 <Connorebauer@gmail.com>
2025-07-13 01:11:37 -0500

    Added UI functionality to rock paper scissors

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 let humanScore = 0;
 let computerScore = 0;
+let rounds = 0;
 
 
 function getComputerChoice(){
@@ -19,20 +20,46 @@ function capitalize(word){
 function playRound(humanChoice, computerChoice){
     if(humanChoice.length == computerChoice.length){
         console.log("Tie Round");
+        result.textContent = "Tie Round";
+        
     }else if(humanChoice.length == 4 && computerChoice.length == 8){
         console.log("You win! " + capitalize(humanChoice) + " beats " + capitalize(computerChoice));
+        result.textContent = "You win! " + capitalize(humanChoice) + " beats " + capitalize(computerChoice);
         humanScore ++;
     }else if(computerChoice.length == 4 && humanChoice.length == 8){
         console.log("You loose! " + capitalize(humanChoice) + " looses to " + capitalize(computerChoice));
+        result.textContent = "You loose! " + capitalize(humanChoice) + " looses to " + capitalize(computerChoice);
         computerScore++;
     }else if(humanChoice.length > computerChoice.length){
         console.log("You win! " + capitalize(humanChoice) + " beats " + capitalize(computerChoice));
+        result.textContent = "You win! " + capitalize(humanChoice) + " beats " + capitalize(computerChoice);
         humanScore ++;
     }else{
         console.log("You loose! " + capitalize(humanChoice) + " looses to " + capitalize(computerChoice));
+        result.textContent = "You loose! " + capitalize(humanChoice) + " looses to " + capitalize(computerChoice);
         computerScore++;
     }
-    
+    score.textContent = `You : ${humanScore} vs ${computerScore} : Computer`;
+    rounds++;
+    gameEnding();
+}
+
+function gameEnding(){
+    if(rounds == 5){
+        rock.disabled = true;
+        scissors.disabled = true;
+        paper.disabled = true;
+        if(humanScore == computerScore){
+            console.log("Tie Game :|");
+            result.textContent = "Tie Game :|";
+        }else if(humanScore > computerScore){
+            console.log("You Win!");
+            result.textContent = "You Win!";
+        }else{
+            console.log("You Loose :(");
+            result.textContent = "You Loose :(";
+        }
+    }
 }
 
 function playGame(){
@@ -49,4 +76,36 @@ function playGame(){
     }
 }
 
-playGame();
+//playGame();
+
+const container = document.querySelector("#container");
+
+// creating the three buttons
+const rock = document.createElement('button');
+const paper = document.createElement('button');
+const scissors = document.createElement('button');
+const result = document.createElement('div');
+const score = document.createElement('div');
+
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+result.textContent = "";
+score.textContent = "";
+
+container.appendChild(rock);
+container.appendChild(paper);
+container.appendChild(scissors);
+container.appendChild(result);
+container.appendChild(score);
+
+rock.addEventListener("click" , function(e){
+    playRound(e.target.textContent, getComputerChoice());
+});
+paper.addEventListener("click" , function(e){
+    playRound(e.target.textContent, getComputerChoice());
+});
+scissors.addEventListener("click" , function(e){
+    playRound(e.target.textContent, getComputerChoice());
+});
+
